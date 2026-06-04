9824957ce77dfcb5ac6ebae1e79799ccf8afa0b9
Yaafi <yaafiandraapple@gmail.com>
2026-06-04 11:25:27 +0700

    .

--- TestingCollab/ContentView.swift
+++ TestingCollab/ContentView.swift
@@ -20,6 +20,8 @@ struct ContentView: View {
         HStack{
             Text("I wanna be")
             Text("Syalalala")
+            Text("Syalalala")
+
         }
         .padding()
         .background(Color.blue)
