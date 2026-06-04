989be007e174a4185d6b9ec90bce87b4de352096
Yaafi <yaafiandraapple@gmail.com>
2026-06-04 10:38:55 +0700

    Initial Commit

---
+++ TestingCollab/TestingCollabApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingCollabApp.swift
+//  TestingCollab
+//
+//  Created by Yaafiandra Rajel Ahsan on 04/06/26.
+//
+
+import SwiftUI
+
+@main
+struct TestingCollabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
